1fd57c43dd491afe69956a8eb7251b16f10d3f28
Shukur Huseynli <bjksukur@gmail.com>
2023-11-08 15:31:58 +0400

    add google provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth, {NextAuthOptions} from "next-auth"
 import  GithubProvider from "next-auth/providers/github"
+import  GoogleProvider from "next-auth/providers/google"
 import { NextRequest } from "next/server";
 
 const authOptions: NextAuthOptions = {
@@ -8,6 +9,10 @@ const authOptions: NextAuthOptions = {
             clientId: process.env.GITHUB_ID ?? "",
             clientSecret: process.env.GITHUB_SECRET ?? ""
         }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID ?? "",
+            clientSecret: process.env.GOOGLE_SECRET ?? ""
+        })
     ],
     secret: process.env.NEXTAUTH_SECRET
 }
