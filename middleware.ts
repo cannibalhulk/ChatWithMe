2acd35cd494aff6b0c24a8137a0e5cf2154bec80
Shukur Huseynli <bjksukur@gmail.com>
2023-11-23 12:09:33 +0400

    fix: `/settings` page is now protected

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,14 @@
 import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/', request.url))
+  if(request.cookies.has("next-auth.session-token")) { // if user logged in 
+    return NextResponse.rewrite(new URL('/settings', request.url))  // i can redirect to the protected page if i want
+
+  } else { // if there is no user logged in
+    return NextResponse.redirect(new URL('/', request.url)) // whenever I open a protected page, it will automatically redirect to the main page which is "/""
+  }
 }
  
 // See "Matching Paths" below to learn more
