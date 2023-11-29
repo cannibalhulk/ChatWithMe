d404cc24df7918c46f6b40106e691ab543253917
Shukur Huseynli <bjksukur@gmail.com>
2023-11-30 00:38:47 +0400

    logo is updated

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,7 +1,11 @@
 "use client";
 import React from "react";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
+import { useTheme } from "next-themes";
 import { Button } from "@nextui-org/react";
+import DarkLogo from '@/public/darkLogo.svg'
+import LightLogo from '@/public/lightLogo.svg'
+import Image  from "next/image"
 import Link from "next/link";
 import UserDropMenu from "./UserDropMenu";
 
@@ -12,16 +16,19 @@ export function AuthButton() {
         <Link href={'/login'}><Button color="default">Log in</Button></Link>
       </div>
     );
-  }
+}
   
   const Navbar = () => {
     const { data: session } = useSession();
+    const {theme} = useTheme();
     
     return (
       <>
         <nav className="dark:bg-black p-3 flex items-center  w-full justify-between sm:px-[100px] min-md:px-[300px] max-md:[400px]">
           <Link href={"/"}>
-            <h1 className="text-2xl font-bold hover:opacity-75">ChatWithMe</h1>
+            {
+              theme === "dark" ? <Image className="hover:opacity-60 duration-100" width={190} src={LightLogo} alt={"light_logo"} /> : <Image className="hover:opacity-60 duration-100" width={190}  src={DarkLogo} alt={"dark_logo"}  />
+            }
           </Link>
           {!session ? <AuthButton /> : <UserDropMenu/> }
           
