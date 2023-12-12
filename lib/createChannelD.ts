a59d46e5e9c5be4b2e1a1e3fe94167ef3a35b184
Shukur Huseynli <bjksukur@gmail.com>
2023-12-12 12:46:55 +0400

    update generating channel ID function

--- lib/createChannelD.ts
+++ lib/createChannelD.ts
@@ -1,4 +1,4 @@
 export function createChannelID(channelName: string) {
     const randomChars = Math.random().toString(36).substring(2, 10);
-    return `${channelName}${randomChars}`;
+    return `${channelName}-${randomChars}`;
 }
