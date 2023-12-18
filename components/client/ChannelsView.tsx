e9dc887003d2a80c72cf08fa358de6dec9fbadb4
Shukur Huseynli <bjksukur@gmail.com>
2023-12-18 23:20:51 +0400

    update layout

--- components/client/ChannelsView.tsx
+++ components/client/ChannelsView.tsx
@@ -17,13 +17,13 @@ export type TChannel = {
 function ChannelsView({ channels }: { channels: TChannel[] }) {
   
   return (
-    <ul className="flex flex-col space-y-5 items-center pb-6">
+    <ul className="flex flex-col space-y-5 items-center pb-6 w-full">
       {channels.map((channels) => (
         <Link key={channels._id} href={`/channels/${channels._id}`}>
           <Badge  isInvisible={isNewChannel(`${channels.createdAt}`)} content="New" placement="top-left"  color="danger" size="sm">
             <li
               key={channels._id}
-              className="pt-3 bg-gradient-to-br dark:from-[#2f2f2f] dark:via-red-200/10 dark:to-[#ffeffe]/30 flex flex-col space-y-2 w-full md:w-[600px] py-2 px-5 rounded-lg border hover:drop-shadow-xl shadow-white transition hover:opacity-70 hover:scale-[0.995]  ease-in-out duration-300 border-[#ffbffb]/30"
+              className="pt-3 bg-gradient-to-br dark:from-[#2f2f2f] dark:via-red-200/10 dark:to-[#ffeffe]/30 flex flex-col space-y-4 min-w-[390px] md:w-[600px] py-2 px-5 rounded-lg border hover:drop-shadow-xl shadow-white transition hover:opacity-70 hover:scale-[0.995]  ease-in-out duration-300 border-[#ffbffb]/30"
             >
               <div className="flex space-y-4 flex-col w-full">
                 <div className="flex justify-between flex-col w-full">
