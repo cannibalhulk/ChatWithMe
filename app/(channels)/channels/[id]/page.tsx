c927088deb6e1d3f85df06892df47d621661df74
Shukur Huseynli <bjksukur@gmail.com>
2023-12-19 11:28:41 +0400

    update `generateMetadata()` function

--- app/(channels)/channels/[id]/page.tsx
+++ app/(channels)/channels/[id]/page.tsx
@@ -1,11 +1,17 @@
 import ChannelView from "@/components/client/ChannelView";
 import { TChannel } from "@/components/client/ChannelsView";
+import { Metadata } from "next";
 import { FC } from "react";
 
-export function generateMetadata({params}: {params: PageProps}) {
+export async function generateMetadata({params}: {params: {id:number}}) {
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/channels?id=${params.id}`,{cache:"no-cache"});
+  const data: TChannel = await res.json();
     return {
-        title: `${params.params.chnl_name}`
-    }
+        title: `${data.chnl_name}`,
+        description: `${data.chnl_desc}`,
+        creator: `${data.createdBy}`,
+        keywords: `${data.category}`,
+    } as Metadata
 }
 interface PageProps {
     params:{
@@ -21,9 +27,10 @@ interface PageProps {
 }
 
 export async function generateStaticParams() {
-  const res = await fetch("/api/channels");
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/channels`,{cache:"no-cache"});
   const data: TChannel[] = await res.json();
   return data.map((channel) => ({
+    _id: channel._id,
     chnl_id: channel.chnl_id,
     chnl_name: channel.chnl_name,
     chnl_desc: channel.chnl_desc,
@@ -37,6 +44,7 @@ const Page: FC<PageProps> = ({params}) => {
   return (
     <main className="pt-10 w-4/5 min-h-screen">
       <section className="flex flex-col mt-15">
+        <h1>{params._id}</h1>
         <ChannelView params={params}/>
       </section>
     </main>
